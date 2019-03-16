7edf1f8f6dc782be6bef3a1a7923604b6d4c5cc9
lixuguang1988 <lixuguang_1988@163.com>
2019-03-16 12:11:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import CommonHeader from './components/CommonHeader';
+import Home from './routes/Home';
+import News from './routes/News';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <CommonHeader />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/news" component={News} />
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
+// This must be the first line in src/index.js
+//开发模式不支持ie9/10
+import 'react-app-polyfill/ie9';
+import '@babel/polyfill';
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+import { LocaleProvider } from 'antd';
+//antd 本地化
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 import './index.css';
+import 'antd/dist/antd.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
+//moment 本地化
+moment.locale('zh-cn');
+
+ReactDOM.render(<Provider store={store}>
+  <LocaleProvider locale={zhCN}>
+    <Router>
+      <App /> 
+    </Router>
+  </LocaleProvider>
+</Provider>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
